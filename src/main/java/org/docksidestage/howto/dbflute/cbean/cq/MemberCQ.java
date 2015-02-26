1c7f7fe85c10b1ef555901fa786caf7b5549bdee
jflute <dbflute@gmail.com>
2015-02-26 23:04:29 +0900

    rename package

---
+++ src/main/java/org/docksidestage/howto/dbflute/cbean/cq/MemberCQ.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.howto.dbflute.cbean.cq;
+
+import org.dbflute.cbean.ConditionQuery;
+import org.dbflute.cbean.sqlclause.SqlClause;
+import org.docksidestage.howto.dbflute.cbean.cq.bs.BsMemberCQ;
+
+/**
+ * The condition-query of MEMBER.
+ * <p>
+ * You can implement your original methods here.
+ * This class remains when re-generating.
+ * </p>
+ * @author DBFlute(AutoGenerator)
+ * @author jflute
+ */
+public class MemberCQ extends BsMemberCQ {
+
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
+    // You should NOT touch with this constructor.
+    /**
+     * Constructor.
+     * @param childQuery Child query as abstract class. (Nullable: If null, this is base instance.)
+     * @param sqlClause SQL clause instance. (NotNull)
+     * @param aliasName My alias name. (NotNull)
+     * @param nestLevel Nest level.
+     */
+    public MemberCQ(ConditionQuery childQuery, SqlClause sqlClause, String aliasName, int nestLevel) {
+        super(childQuery, sqlClause, aliasName, nestLevel);
+    }
+
+    // ===================================================================================
+    //                                                                       Arrange Query
+    //                                                                       =============
+    // You can make your arranged query methods here. e.g. public void arrangeXxx()
+    /**
+     * Arrange the query for selecting service members.
+     * o starts 'S'
+     * o status 'Formalized'
+     * o exists the special product
+     */
+    public void arrangeSpecialServiceMember() {
+        final Integer specialProductId = 3;
+        setMemberName_LikeSearch("S", op -> op.likePrefix());
+        setMemberStatusCode_Equal_Formalized();
+        existsPurchase(purCB -> {
+            purCB.query().setProductId_Equal(specialProductId);
+        });
+    }
+}
---
+++ src/main/java/org/docksidestage/howto/dbflute/exentity/Purchase.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.howto.dbflute.exentity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.docksidestage.howto.dbflute.bsentity.BsPurchase;
+
+/**
+ * The entity of PURCHASE.
+ * <p>
+ * You can implement your original methods here.
+ * This class remains when re-generating.
+ * </p>
+ * @author DBFlute(AutoGenerator)
+ * @author jflute
+ */
+public class Purchase extends BsPurchase {
+
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    /** Serial version UID. (Default) */
+    private static final long serialVersionUID = 1L;
+    /** SUM_PAYMENT_AMOUNT: Derived Referrer Alias. */
+    public static final String ALIAS_sumPaymentAmount = "SUM_PAYMENT_AMOUNT";
+    /** SUM_PAYMENT_AMOUNT: (Derived Referrer) */
+    protected BigDecimal _sumPaymentAmount;
+
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
+    /**
+     * [get] SUM_PAYMENT_AMOUNT: (Derived Referrer)
+     * @return The value of the column 'SUM_PAYMENT_AMOUNT'. (NullAllowed)
+     */
+    public BigDecimal getSumPaymentAmount() {
+        return _sumPaymentAmount;
+    }
+
+    /**
+     * [set] SUM_PAYMENT_AMOUNT: (Derived Referrer)
+     * @param sumPaymentAmount The value of the column 'SUM_PAYMENT_AMOUNT'. (NullAllowed)
+     */
+    public void setSumPaymentAmount(BigDecimal sumPaymentAmount) {
+        _sumPaymentAmount = sumPaymentAmount;
+    }
+
+    // ===================================================================================
+    //                                                             for test: Non-Specified
+    //                                                             =======================
+    public Integer xznocheckGetMemberId() {
+        return _memberId;
+    }
+
+    public Integer xznocheckGetProductId() {
+        return _productId;
+    }
+
+    public LocalDateTime xznocheckGetPurchaseDatetime() {
+        return _purchaseDatetime;
+    }
+
+    public Integer xznocheckGetPurchaseCount() {
+        return _purchasePrice;
+    }
+
+    public Integer xznocheckGetPurchasePrice() {
+        return _purchasePrice;
+    }
+
+    public Integer xznocheckGetPaymentCompleteFlg() {
+        return _paymentCompleteFlg;
+    }
+}
