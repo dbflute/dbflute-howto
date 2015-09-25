9f8cd5cfe8978fa1e8b2c0565a42ea406ec87bb4
jflute <dbflute@gmail.com>
2015-09-25 16:43:42 +0900

    remove version_no from withdrawal as ERD

--- src/main/java/org/docksidestage/howto/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
+++ src/main/java/org/docksidestage/howto/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
@@ -78,7 +78,7 @@ public class VendorIdentityOnlyDbm extends AbstractDBMeta {
     // ===================================================================================
     //                                                                         Column Info
     //                                                                         ===========
-    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_7CB4E4E4_9E35_49FC_ACB0_8C72FB7FB756", false, null, null, null, null, null, false);
+    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_B3D5EF4C_16FF_42AC_812D_0A4397277EF6", false, null, null, null, null, null, false);
 
     /**
      * IDENTITY_ONLY_ID: {PK, ID, NotNull, BIGINT(19)}
